18399a69a20e71fa801deaa106df69398fb7b915
mhooonn <max.hallenberg@hotmail.com>
2026-04-15 14:31:31 +0300

    home page

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('handlebars', exphbs.engine({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
 // home
 app.get('/', (req,res) => {
     res.render('index', {
         title: "games ltd"
     });
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
